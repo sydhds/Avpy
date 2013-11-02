222932b9756cffc881b82f2b38995b3abbf0c8fa
sydh <sydhds@gmail.com>
2013-11-02 19:08:23 +0100

    mediaInfo: improve output

--- examples/mediaInfo/mediaInfo.py
+++ examples/mediaInfo/mediaInfo.py
@@ -1,22 +1,44 @@
 #!/usr/bin/env python
 
-import sys
 from pyav import Media
+
+def printSep(ch='='):
+    print ch*25
 
 if __name__ == '__main__':
 
+    # cmdline
+    from optparse import OptionParser
+
+    usage = "usage: %prog -m foo.avi"
+    parser = OptionParser(usage=usage)
+    parser.add_option('-m', '--media', 
+            help='play media')
+    (options, args) = parser.parse_args() 
+
     # print a dict with available formats to encode and decode
     print 'available formats:'
-    print Media.formats()
+    formats = Media.formats()
+    print formats
 
     # print mp3 codec information
-    print 'codec information:'
+    codecName = 'mp3'
+    printSep()
+    print '%s (%s) information:' % (codecName, formats['decoding'].get(codecName, ''))
     print Media.codecInfo('mp3')
     
     # open
-    if len(sys.argv) > 1:
-        m = Media(sys.argv[1])
-        print 'file info:'
-        print m.info()
+    if options.media:
+        m = Media(options.media)
+        printSep()
+        print '%s info:' % options.media
+        infoDict = m.info()
+
+        print 'stream(s):'
+        for stream in infoDict['stream']:
+            print stream
+
     else:
         print 'please provide a movie or sound file'
+
+
