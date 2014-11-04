9708cbd57ceeff5d76b541784aa5ceb20c8fa6d6
sydh <sydhds@gmail.com>
2014-11-04 21:05:11 +0100

    Add supported formats and codecs in libInfo

--- examples/misc/libInfo.py
+++ examples/misc/libInfo.py
@@ -4,6 +4,30 @@ from pyav import avMedia
 
 if __name__ == '__main__':
 
+    # cmdline
+    from optparse import OptionParser
+
+    usage = "usage: %prog -f"
+    parser = OptionParser(usage=usage)
+    parser.add_option('-f', '--formats',
+            action='store_true',
+            help='print supported formats')
+    parser.add_option('-c', '--codecs',
+            default='all',
+            help='print supported codecs (default: %default): all, video, audio, subtitle')
+    (options, args) = parser.parse_args()
+    
     print('Using pyav version %s' % avMedia.__version__)
     pprint(avMedia.versions())
+    
+    if options.formats:
+        pprint(avMedia.Media.formats())
+
+    if options.codecs:
+        codecs = avMedia.Media.codecs()
+
+        if options.codecs in ['audio', 'video', 'subtitle']:
+            pprint(codecs[options.codecs])
+        else:
+            pprint(codecs)
 
